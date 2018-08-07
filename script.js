57d8b3ee75669749bd2115f86301db1312f443cd
FUSSE FAUSTO <43719026@ort>
2018-08-07 15:11:21 -0300

    yoda

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-function getLocationJSON() {
-	
-	$.get('http://jsonip.com', function (res) {
+var keyIpStack = "01b795cb905222891a979bd96c96799c";
+var ip = "181.30.51.38";
+var traduccion = "";
 
-        $('p').html('IP Address is: ' + res.ip);
+$('#submitYoda').click(function() {
+	$('span#traduccion').html(getYoda($('input#yoda').val()));
+});
 
-    });
-
-	var ip = "181.30.51.38";
-	var key = "01b795cb905222891a979bd96c96799c";
-
+function getLocationJSON(key, ip) {
 	$.ajax({
 		method:'GET',
 		//url:'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139',
-		url: 'http://api.ipstack.com/'+ip+'?access_key='+key,
+		url: 'http://api.ipstack.com/'+ip+'?access_key='+keyIpStack,
 		dataType:'json',
-		success:onSuccess,
-		error:onError
+		success:function onSuccess (jsonReturn){
+			$('p').html(JSON.stringify(jsonReturn));
+		},
+		error:function onError (){
+			alert('Error obteniendo JSON');
+		}
 	})
-	
-	function onSuccess (jsonReturn){
-		//alert(JSON.stringify(jsonReturn));
-		var pais = jsonReturn.response.country_name;
-		alert(pais);
-	}
-
-	function onError (){
-		alert('Error obteniendo JSON');
-	}
-	
 }
+//getLocationJSON(keyIpStack, ip);
+	
+function getYoda(text){
+	var params = {"text":text,};
+	
+	$.ajax({
+		url: 'http://api.funtranslations.com/translate/yoda.json?'+ $.param(params),
+		type: 'GET',
+		dataType: 'json',
+	})
+	.done(function(data) {
+		$('span').html(data.contents.translated);
+		traduccion = "gomes";
+	})
+	.fail(function() {
+		alert("Error obteniendo json de yoda");
+	});
 
-getLocationJSON();
+	return "Traduccion: " + traduccion;
+}
