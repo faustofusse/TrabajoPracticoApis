c716b65537d7106cf70f2fad78d50e36e1690f78
FUSSE FAUSTO <43719026@ort>
2018-08-15 17:17:08 -0300

    .

--- scripts/main.js
+++ scripts/main.js
@@ -1,75 +1,33 @@
-initialize();
-
-// ---------------- VARIABLES --------------------------
-
 var diarios = [{id:"cnn",baseColor:"",colorHover:""},
     	{id:"daily-mail",baseColor:"",colorHover:""},
     	{id:"bbc-news",baseColor:"",colorHover:""},
     	{id:"marca",baseColor:"",colorHover:""},
     	{id:"the-new-york-times",baseColor:"#263238",colorHover:"#1565c0"},
     	{id:"fox-sports",baseColor:"",colorHover:""}];
+var diarioActivo = diarios[0];
+var key = "d1eae06defad4bb29383968c90e85a4b";
+var url = "https://newsapi.org/v2/everything?" + "apiKey=" + key;
+var fecha;
+var noticia = $('div.noticia');
 var date = new Date();
 var day = date.getDate(); var daysPast = 0;
 var month = date.getMonth()+1; var monthsPast = 0;
 var year = date.getFullYear(); var yearsPast = 0;
+$('div.noticia').remove();
+rellenarContenido();
 
-var diarioActivo = diarios[0];
-var idioma = "en";
-var noticia = $('div.noticia');
-var key = "d1eae06defad4bb29383968c90e85a4b";
-var fecha = getYear()+"-"+getMonth()+"-"+getDay();
-
-var parameters = {
-	"apiKey": key,
-	"sources": diarioActivo.id,
-	"language": idioma,
-	"to": fecha
-	//"q":"bitcoin",
-};
-
-var ultimaNoticia;
-var scrollTop;
-var elementOffset;
-var distance;
-
-
-// ---------------- EVENTS --------------------------
-
-$(window).scroll(function(event) {
-	scrollTop = $(window).scrollTop();
-    elementOffset = $(ultimaNoticia).offset().top;
-    distanceMinima = (elementOffset - scrollTop - (ultimaNoticia.height()*2));
-	if (distanceMinima <= 0){
-		//alert('recargar');
-		agregarContenido();
-	}
-});
-	
 $('header nav div.diarios button').click(function() {
-	parameters.sources = $(this).attr('id');
-	actualizarContenido();
-}); 
+	//diarioActivo = findDiarioBySource($(this).attr('id'));
+	//alert(diarioActivo.id);
+	var id = $(this).attr('id');
+	diarioActivo = findDiarioBySource(id);
+	alert(diarioActivo.id);
+});
 
-
-
-// ---------------- FUNCTIONS --------------------------
-
-function initialize(){
-	actualizarContenido();
-	$('button#'+diarioActivo.id).css('background-color', diarioActivo.colorHover);	
-}
-	
-function actualizarContenido () {
-	
-	$('div.noticia').remove();
-	$.ajax({
-		//url: 'https://newsapi.org/v2/sources?' + key,
-		//url: 'https://newsapi.org/v2/top-headlines?' + $.param(parameters),
-		url: 'https://newsapi.org/v2/everything?' + $.param(parameters),
-		type: 'GET',
-		dataType: 'json',
-	})
-	.done(function(data) {
+function rellenarContenido(){
+	fecha = getYear() + "-" + getMonth() + "-" + getDay();
+	avanzarDia();
+	$.get(url, {sources:diarioActivo.id, language:"en"}, function (data, status) {
 		console.log(JSON.stringify(data));
 		for (var i = 0; i < data.articles.length; i++) {
 			//alert(JSON.stringify(data.articles[i]));
@@ -85,50 +43,13 @@ function actualizarContenido () {
 		}
 		ultimaNoticia = $('div.noticia:last');
 		ultimaNoticia.css('background-color', 'blue');
-
-	})
-	.fail(function() {
-		alert("Error obtaining JSON");
-	});
+	}, 'json');
 }
 
-function agregarContenido(){
-
-	avanzarDia();
-	$.ajax({
-		//url: 'https://newsapi.org/v2/sources?' + key,
-		//url: 'https://newsapi.org/v2/top-headlines?' + $.param(parameters),
-		url: 'https://newsapi.org/v2/everything?' + $.param(parameters),
-		type: 'GET',
-		dataType: 'json',
-	})
-	.done(function(data) {
-		//console.log(JSON.stringify(data));
-		for (var i = 0; i < data.articles.length; i++) {
-			//alert(JSON.stringify(data.articles[i]));
-			var clon = noticia.clone();
-			var fuente = data.articles[i].source.name + " - " + data.articles[i].publishedAt;
-			var link = data.articles[i].url;
-			clon.find('h2').html(data.articles[i].title);
-			clon.find('h3').html(data.articles[i].description);
-			clon.find('img').attr('src',data.articles[i].urlToImage);
-			clon.find('span').html(fuente);
-			clon.click(function (){alert(link);});
-			clon.appendTo('main'); 
-		}
-		ultimaNoticia = $('div.noticia:last');
-		ultimaNoticia.css('background-color', 'blue');
-
-	})
-	.fail(function() {
-		alert("Error obtaining JSON");
-	});
-}
-
-function findButtonById(id){
-	for (var b in botones){
-		if (id == b.id){
-			return b;
+function findDiarioBySource(source){
+	for (var d in diarios){
+		if (source == d.id){
+			return d;
 		}
 	}
 	return null;
